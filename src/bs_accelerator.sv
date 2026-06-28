c1f98aea0a267ff4fddab33c6200381f53cb248b
andychiem19 <andy.chiem@gmail.com>
2026-06-28 11:49:55 -0400

    Fix horrendous block diagram

--- src/bs_accelerator.sv
+++ src/bs_accelerator.sv
@@ -6,11 +6,7 @@ module bs_accelerator (
     input logic         packet_end,
 
     output logic [31:0] delta,
-    output logic [31:0] gamma,
-    output logic [31:0] vega,
-    output logic [31:0] theta,  // shoutout thetagang
-    output logic [31:0] rho,
-    output logic        result_valid
+    output logic        valid
 );
 
 endmodule
